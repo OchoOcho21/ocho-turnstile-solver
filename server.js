656d37051282ebf6501fa61d9363f1c8d1c41525
OchoOcho21 <ochoocho21162@gmail.com>
2025-08-09 08:02:05 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Solver = require('./solver');
+const app = express();
+
+app.use(express.json());
+
+app.post('/solve', async (req, res) => {
+    const { url, sitekey, invisible } = req.body;
+    if (!url || !sitekey) return res.status(400).json({ error: 'Missing url or sitekey' });
+
+    const solver = new Solver();
+    const token = await solver.solve(url, sitekey, invisible || false);
+    res.json({ token });
+});
+
+app.get('/', (req, res) => res.send('Turnstile Solver is running.'));
+
+app.listen(10000, () => console.log('Server started on port 10000'));
